c8c1b1c22373248c56e31fdbebecca32357db52d
Thomas Schaller <torkleyy@gmail.com>
2018-08-07 18:09:37 +0200

    Check heterogeneous sequence doesn't become a list

--- src/ser/value.rs
+++ src/ser/value.rs
@@ -20,3 +20,30 @@ impl Serialize for Value {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use de::from_str;
+    use ser::to_string;
+
+    #[test]
+    fn check_tuple_het() {
+        let s = to_string(&Value::Seq(vec![
+            Value::Bool(false),
+            Value::Char('c'),
+        ])).unwrap();
+
+        assert_eq!(from_str::<(bool, char)>(&s).unwrap(), (false, 'c'));
+    }
+
+    #[test]
+    fn check_list_stays_list() {
+        let s = to_string(&Value::Seq(vec![
+            Value::Bool(false),
+            Value::Bool(true),
+        ])).unwrap();
+
+        assert_eq!(from_str::<Vec<bool>>(&s).unwrap(), vec![false, true]);
+    }
+}
