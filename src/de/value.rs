0d2e1a871083bd5c2631f150bc6bbb4db68e3e48
Egor Larionov <egor.larionov@gmail.com>
2020-04-21 12:01:06 -0700

    Fix from_str errors caused by a clippy fix
    The issue was that the from_str implementation on Value was moved to the
    FromStr trait as recommended by Clippy. This caused many of the tests to
    fail since now the FromStr trait must be in scope. Instead of bringing
    FromStr into scope everywhere, however, the `Value::from_str(_)` calls
    were replaced with `_.parse()` calls as recommended by the std lib
    documentation.

--- src/de/value.rs
+++ src/de/value.rs
@@ -168,9 +168,10 @@ impl<'de> Visitor<'de> for ValueVisitor {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::str::FromStr;
 
     fn eval(s: &str) -> Value {
-        Value::from_str(s).expect("Failed to parse")
+        s.parse().expect("Failed to parse")
     }
 
     #[test]
