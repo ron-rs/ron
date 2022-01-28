648fc5e35850be3c69373a33395962513ced1701
Momo Langenstein <momo.langenstein@helsinki.fi>
2022-01-28 17:54:44 +0000

    Increased robustness for changed `NonZeroU32` de error in test

--- tests/203_error_positions.rs
+++ tests/203_error_positions.rs
@@ -1,6 +1,7 @@
 use std::num::NonZeroU32;
 
 use ron::error::{Error, ErrorCode, Position};
+use serde::de::{value::Error as DeError, Deserialize, IntoDeserializer};
 
 #[derive(Debug, serde::Deserialize, PartialEq)]
 enum Test {
@@ -23,7 +24,11 @@ fn test_error_positions() {
     assert_eq!(
         ron::from_str::<Test>("TupleVariant(1, 0)"),
         Err(Error {
-            code: ErrorCode::Message(String::from("expected a non-zero value")),
+            code: ErrorCode::Message(
+                NonZeroU32::deserialize(IntoDeserializer::<DeError>::into_deserializer(0_u32))
+                    .unwrap_err()
+                    .to_string()
+            ),
             position: Position { line: 1, col: 18 },
         })
     );
@@ -31,7 +36,11 @@ fn test_error_positions() {
     assert_eq!(
         ron::from_str::<Test>("StructVariant(a: true, b: 0, c: -42)"),
         Err(Error {
-            code: ErrorCode::Message(String::from("expected a non-zero value")),
+            code: ErrorCode::Message(
+                NonZeroU32::deserialize(IntoDeserializer::<DeError>::into_deserializer(0_u32))
+                    .unwrap_err()
+                    .to_string()
+            ),
             position: Position { line: 1, col: 28 },
         })
     );
