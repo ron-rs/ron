706162d4b60124a44b1bc2f1579f7e5eb97f4b1f
Juniper Langenstein <50025784+MomoLangenstein@users.noreply.github.com>
2022-04-01 07:35:08 +0300

    Fix \0 in string on nightly + enable weekly CI (#373)

--- src/parse.rs
+++ src/parse.rs
@@ -811,6 +811,7 @@ impl<'a> Bytes<'a> {
             b'n' => '\n',
             b'r' => '\r',
             b't' => '\t',
+            b'0' => '\0',
             b'x' => self.decode_ascii_escape()? as char,
             b'u' => {
                 self.expect_byte(b'{', Error::InvalidEscape("Missing { in Unicode escape"))?;
