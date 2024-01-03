521d91c35e41b5c346fd69668fae4ce2016b516f
SylvKT <contact@sylv.gay>
2024-01-02 22:41:23 -0500

    prepare adding `explicit_struct_names` extension

--- src/extensions.rs
+++ src/extensions.rs
@@ -7,6 +7,8 @@ bitflags::bitflags! {
         const UNWRAP_NEWTYPES = 0x1;
         const IMPLICIT_SOME = 0x2;
         const UNWRAP_VARIANT_NEWTYPES = 0x4;
+        /// During deserialization, this extension requires that structs' names are stated explicitly.
+        const EXPLICIT_STRUCT_NAMES = 0x8;
     }
 }
 // GRCOV_EXCL_STOP
@@ -19,6 +21,7 @@ impl Extensions {
             "unwrap_newtypes" => Some(Extensions::UNWRAP_NEWTYPES),
             "implicit_some" => Some(Extensions::IMPLICIT_SOME),
             "unwrap_variant_newtypes" => Some(Extensions::UNWRAP_VARIANT_NEWTYPES),
+            "explicit_struct_names" => Some(Extensions::EXPLICIT_STRUCT_NAMES),
             _ => None,
         }
     }
@@ -40,19 +43,25 @@ mod tests {
         assert_eq!(ext, ext2);
     }
 
+    // todo: maybe make a macro for this?
     #[test]
     fn test_extension_serde() {
         roundtrip_extensions(Extensions::default());
         roundtrip_extensions(Extensions::UNWRAP_NEWTYPES);
         roundtrip_extensions(Extensions::IMPLICIT_SOME);
         roundtrip_extensions(Extensions::UNWRAP_VARIANT_NEWTYPES);
+        roundtrip_extensions(Extensions::EXPLICIT_STRUCT_NAMES);
         roundtrip_extensions(Extensions::UNWRAP_NEWTYPES | Extensions::IMPLICIT_SOME);
         roundtrip_extensions(Extensions::UNWRAP_NEWTYPES | Extensions::UNWRAP_VARIANT_NEWTYPES);
+        roundtrip_extensions(Extensions::UNWRAP_NEWTYPES | Extensions::EXPLICIT_STRUCT_NAMES);
         roundtrip_extensions(Extensions::IMPLICIT_SOME | Extensions::UNWRAP_VARIANT_NEWTYPES);
+        roundtrip_extensions(Extensions::IMPLICIT_SOME | Extensions::EXPLICIT_STRUCT_NAMES);
+        roundtrip_extensions(Extensions::UNWRAP_VARIANT_NEWTYPES | Extensions::EXPLICIT_STRUCT_NAMES);
         roundtrip_extensions(
             Extensions::UNWRAP_NEWTYPES
                 | Extensions::IMPLICIT_SOME
-                | Extensions::UNWRAP_VARIANT_NEWTYPES,
+                | Extensions::UNWRAP_VARIANT_NEWTYPES
+                | Extensions::EXPLICIT_STRUCT_NAMES,
         );
     }
 }
