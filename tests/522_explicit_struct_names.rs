c7c4a8a229c321e7ecf4e2916d7051d154aa2d3c
SylvKT <contact@sylv.gay>
2024-01-04 11:52:04 -0500

    add tuple struct test, use all codepaths

--- tests/522_explicit_struct_names.rs
+++ tests/522_explicit_struct_names.rs
@@ -5,13 +5,17 @@ use serde_derive::Deserialize;
 struct Id(u32);
 
 #[derive(Debug, Deserialize)]
+struct Position(f32, f32);
+
+#[derive(Debug, Deserialize)]
 struct Foo {
     #[allow(unused)]
     pub id: Id,
+    #[allow(unused)]
+    pub position: Position,
 }
 
 const EXPECT_ERROR_MESSAGE: &'static str = "expected `Err(Error::ExpectedStructName)`, deserializer returned `Ok`";
-const INCORRECT_ERROR_MESSAGE: &'static str = "expected error ExpectedStructName, found";
 
 #[test]
 fn explicit_struct_names() {
@@ -19,25 +23,31 @@ fn explicit_struct_names() {
         .with_default_extension(Extensions::EXPLICIT_STRUCT_NAMES);
 
     // phase 1 (regular structs)
-    let content = r#"(
+    let content_regular = r#"(
         id: Id(3),
+        position: Position(0.0, 8.72),
     )"#;
-    let foo = options.from_str::<Foo>(content);
-    match foo.expect_err(EXPECT_ERROR_MESSAGE).code {
-        Error::ExpectedStructName(_) => {},
-        err => panic!("{INCORRECT_ERROR_MESSAGE} \"{err}\""),
-    }
+    let foo = options.from_str::<Foo>(content_regular);
+    assert_eq!(foo.expect_err(EXPECT_ERROR_MESSAGE).code, Error::ExpectedStructName("Foo".to_string()));
 
     // phase 2 (newtype structs)
-    let content = r#"Foo(
+    let content_newtype = r#"Foo(
         id: (3),
+        position: Position(0.0, 8.72),
     )"#;
-    let foo = options.from_str::<Foo>(content);
-    match foo.expect_err(EXPECT_ERROR_MESSAGE).code {
-        Error::ExpectedStructName(_) => {},
-        err => panic!("{INCORRECT_ERROR_MESSAGE} \"{err}\""),
-    }
+    let foo = options.from_str::<Foo>(content_newtype);
+    assert_eq!(foo.expect_err(EXPECT_ERROR_MESSAGE).code, Error::ExpectedStructName("Id".to_string()));
+    
+    // phase 3 (tuple structs)
+    let content_tuple = r#"Foo(
+        id: Id(3),
+        position: (0.0, 8.72),
+    )"#;
+    let foo = options.from_str::<Foo>(content_tuple);
+    assert_eq!(foo.expect_err(EXPECT_ERROR_MESSAGE).code, Error::ExpectedStructName("Position".to_string()));
 
-    // phase 3 (test without this extension)
-    let _foo = from_str::<Foo>(content).unwrap();
+    // phase 4 (test without this extension)
+    let _foo = from_str::<Foo>(content_regular).unwrap();
+    let _foo = from_str::<Foo>(content_newtype).unwrap();
+    let _foo = from_str::<Foo>(content_tuple).unwrap();
 }
