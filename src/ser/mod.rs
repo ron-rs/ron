5d450db788dedf331d9f58c82f8305f8d71d1759
Mewily <tstd36@yahoo.fr>
2025-10-23 16:01:11 +0200

    extract serializer

--- src/ser/mod.rs
+++ src/ser/mod.rs
@@ -446,6 +446,12 @@ impl<W: fmt::Write> Serializer<W> {
         })
     }
 
+    /// Unwrap the `Writer` from the `Serializer`.
+    #[inline]
+    pub fn into_inner(self) -> W {
+        self.output
+    }
+
     fn separate_tuple_members(&self) -> bool {
         self.pretty
             .as_ref()
